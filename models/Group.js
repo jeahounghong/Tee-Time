d6e24c7a2dc2c1139e088c76ffb814641997d8b3
“Jeahoung <jeahounghong@gmail.com>
2022-08-29 12:47:38 -0400

    set up Group model

--- models/Group.js
+++ models/Group.js
@@ -2,7 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GroupSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    ownerId: {
+        type: Number,
+        required: true 
+    },
+    users: {
+        type: Array
+    },
+    events: {
+        type: Array
+    },
+    description: {
+        type: String
+    },
+    location: {
+        city: {
+            type: String
+        },
+        state: {
+            type: String
+        }
+    }
+    // Image key for when we set up AWS 
 })
 
 module.exports = Group = mongoose.model('Group', GroupSchema)
