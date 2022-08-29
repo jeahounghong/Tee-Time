087a1436779f5a1d17b8cfa41c144cbb3aa4c6e3
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-29 06:37:49 -0700

    created skeleton for components

---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import splash from './splash/splash';
+
+const App = () => {
+    <div>
+        <Route path='/' component={splash} />
+    </div>
+};
+
+export default App;
---
+++ frontend/src/components/splash/splash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const splash = () => {
+  return (
+    <div>splash</div>
+  )
+}
+
+export default splash;
