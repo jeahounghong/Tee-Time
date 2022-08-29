7b4b5db8701860710fa29e79ad5850c149046fc9
Michael Shen <michaelshen85@gmail.com>
2022-08-29 16:10:25 -0400

    stylized image for login/signup

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -49,7 +49,7 @@ class LoginForm extends React.Component {
                         </div>
                     </form>
                 </div>
-                <div className="session-form-image">
+                <div className="session-form-image" id="splash-img">
                     <img src="https://cdn.dribbble.com/userupload/3079475/file/original-244e5f4b6e1e4476050c9b83515c74fd.jpg?compress=1&resize=1600x1200" alt="" />
                 </div>
             </div>
