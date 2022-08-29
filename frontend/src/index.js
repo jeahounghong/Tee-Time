7b764c2be255aba0374367a72d5da92129112d8d
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-29 10:23:57 -0700

    took out main.scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import {logout} from './actions/session_actions';
-import "./main.scss";
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
