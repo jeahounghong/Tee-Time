c4b3cc518b424de93b981e67b05a291b52b782a7
Michael Shen <michaelshen85@gmail.com>
2022-08-29 17:04:56 -0400

    Add events page

--- frontend/src/components/events/events.jsx
+++ frontend/src/components/events/events.jsx
@@ -1,21 +1,37 @@
 import NavbarContainer from '../navbar/navbar_container';
+import EventItemContainer from './event_item_container';
+import '../../stylesheets/events.css';
 import React from 'react';
 
 class Events extends React.Component {
     constructor(props) {
         super(props);
     }
-
+    
+    componentDidMount() {
+        document.querySelector('body').style.backgroundColor = '#f3f2ee';
+    }
     render() {
         return (
-            <div id="events-page">
+            <div>
                 <NavbarContainer />
-                <div id="events-container">
-                    <ul>
-                        {Object.values(this.props.events).map((event, i) => {
-                            return <EventItem event={event} />
-                        })}
-                    </ul>
+                <div id="events-page">
+                    <div id="events-container">
+                        <p id="events-slogan">Upcoming Events</p>
+                        {/* <ul>
+                            {Object.values(this.props.events).map((event, i) => {
+                                return <EventItem event={event} />
+                            })}
+                        </ul> */}
+                        <ul>
+                            <EventItemContainer />
+                        </ul>
+
+                    </div>
+
+                    <div id="map">
+                        MAP GOES HERE
+                    </div>
                 </div>
             </div>
         )
