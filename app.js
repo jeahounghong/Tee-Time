b1428b332580a2fc2e7081494f7e1b3617fd83ce
“Jeahoung <jeahounghong@gmail.com>
2022-08-29 09:29:38 -0400

    connect to db

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+const db = require(`./config/keys`).mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
