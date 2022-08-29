31b260804e4dd559ecb926aa5f8330d2be6f9c58
Torben Ginsberg <tginsberg8@gmail.com>
2022-08-29 09:49:24 -0400

    set up user model

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const users = require("./routes/api/users");
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+};
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
