fbfb9de6bb023c4c2ca32c2deb09cdb5b5754b1a
“Jeahoung <jeahounghong@gmail.com>
2022-08-29 16:38:00 -0400

    create Course model

--- validation/groups.js
+++ validation/groups.js
@@ -14,27 +14,7 @@ module.exports = function validateGroupInput(data) {
     if (Validator.isEmpty(data.description)){
         errors.description = 'Description field is required';
     }
-
-    // if (!data.ownerId){
-    //     errors.ownerId = 'Groups must have an owner'
-    // }
-
-
-    // data.email = validText(data.email) ? data.email : '';
-    // data.password = validText(data.password) ? data.password : '';
-  
-    // if (!Validator.isEmail(data.email)) {
-    //   errors.email = 'Email is invalid';
-    // }
-  
-    // if (Validator.isEmpty(data.email)) {
-    //   errors.email = 'Email field is required';
-    // }
-  
-    // if (Validator.isEmpty(data.password)) {
-    //   errors.password = 'Password field is required';
-    // }
-  
+    
     return {
       errors,
       isValid: Object.keys(errors).length === 0
