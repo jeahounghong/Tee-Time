14d6ca93f180e4c2896ea1d5e35e7a6dca56a03c
“Jeahoung <jeahounghong@gmail.com>
2022-08-30 00:47:58 -0400

    complete event actions

--- frontend/src/actions/event_actions.js
+++ frontend/src/actions/event_actions.js
@@ -8,3 +8,25 @@ export const receiveEvent = (event) => ({
     type: RECEIVE_EVENT,
     event
 })
+
+export const receiveEvents = (events) => ({
+    type: RECEIVE_EVENTS,
+    events
+})
+
+export const removeEvent = (eventId) => ({
+    type: REMOVE_EVENT,
+    eventsId
+})
+
+export const fetchEvent = (eventId) => dispatch => EventApiUtil.getEvent(eventId)
+    .then(event => dispatch(receiveEvent(event)))
+
+export const createEvent = (event) => dispatch => EventApiUtil.createEvent(event)
+    .then(event => dispatch(receiveEvent(event)))
+
+export const updateEvent = (event) => dispatch => EventApiUtil.updateEvent(event)
+    .then(event => dispatch(receiveEvent(event)));
+
+export const deleteEvent = (eventId) => dispatch => EventApiUtil.deleteEvent(eventId)
+    .then(() => dispatch(removeEvent(eventId)))
