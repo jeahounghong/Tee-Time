3ba79a595993e892a9cdd36d709de9460ecd32a8
“Jeahoung <jeahounghong@gmail.com>
2022-08-30 00:56:24 -0400

    add event reducer to entities

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import groups from './groups_reducer';
 import courses from './courses_reducer';
+import events from './events_reducer';
 
 
 const EntitiesReducer = combineReducers({
     groups,
-    courses
+    courses,
+    events
 });
 
 export default EntitiesReducer;
