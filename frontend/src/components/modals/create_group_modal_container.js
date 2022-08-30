0ae5de88455a784a354e5e6809ccc0fc04560846
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-30 16:41:43 -0700

    group modal is working but there is issue with certain data persisting to backend

--- frontend/src/components/modals/create_group_modal_container.js
+++ frontend/src/components/modals/create_group_modal_container.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 import CreateGroupModal from "./create_group_modal";
 import { fetchUsers } from "../../actions/user_actions";
-import { createGroup } from "../../util/group_api_util";
+import { createGroup } from "../../actions/group_actions";
 const mapStateToProps = state => {
     return {
         allUsers: Object.values(state.entities.users),
