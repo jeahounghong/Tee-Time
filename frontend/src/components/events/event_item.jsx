50c496c9ae4ba8fe9b122fe4945e7b4075e1e70f
Michael Shen <michaelshen85@gmail.com>
2022-08-31 01:38:58 -0400

    Style event item more

--- frontend/src/components/events/event_item.jsx
+++ frontend/src/components/events/event_item.jsx
@@ -1,6 +1,6 @@
 import '../../stylesheets/event_item.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPersonWalkingArrowRight, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faPersonWalkingArrowRight, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 import EditEventModalContainer from '../modals/edit_event_modal_container';
 import React from 'react';
@@ -14,11 +14,13 @@ class EventItem extends React.Component {
     }
 
     attendEvent() {
-        if (Object.values(this.props.event.users.length) < this.props.event.eventSize) {
-            if (this.props.event.users.indexOf(this.props.currentUser) === -1) {
-                this.props.users[this.props.currentUser.id].events.joinedEvents.push(this.props.event);
-                this.props.event.users.push(this.props.currentUser);
-            }
+        if (Object.values(this.props.event.users.length) < this.props.event.eventSize &&
+        this.props.event.users.indexOf(this.props.currentUser) === -1) {
+            this.props.users[this.props.currentUser.id].events.joinedEvents.push(this.props.event);
+            this.props.event.users.push(this.props.currentUser);
+        } else {
+            let idx = this.props.event.users.indexOf(this.props.currentUser);
+            this.props.event.users = this.props.event.users.splice(0, idx).concat(this.props.event.users.splice(idx));
         }
         // force rerender here with setState
         this.setState({editing: this.state.editing})
@@ -76,8 +78,17 @@ class EventItem extends React.Component {
                         <div className="suggested-event">Suggested</div>
                         <div className="user-actions">
                             <div className="event-action" onClick={this.attendEvent} >
-                                <p>Attend event</p>
-                                <FontAwesomeIcon icon={faPersonWalkingArrowRight}></FontAwesomeIcon>
+                                {this.props.event.users.indexOf(this.props.currentUser) > -1 ?
+                                <div className="leave-event">
+                                    <p className="leave-event-p">Leave event</p>
+                                    <FontAwesomeIcon icon={faArrowRightFromBracket}></FontAwesomeIcon>
+                                </div>
+                                : 
+                                <div className="attend-event">
+                                    <p className="attend-event-p">Attend event</p>
+                                    <FontAwesomeIcon icon={faPersonWalkingArrowRight}></FontAwesomeIcon>
+                                </div>
+                                }
                             </div>
                             <div onClick={() => this.setState({editing: !this.state.editing})} className="event-actions-toggle">
                                 <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
--- frontend/src/components/events/event_item_container.jsx
+++ frontend/src/components/events/event_item_container.jsx
@@ -1,3 +1,4 @@
+import { deleteEvent } from '../../actions/event_actions';
 import { connect } from 'react-redux';
 import EventItem from './event_item';
 
@@ -7,7 +8,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-
+    // deleteEvent: event => dispatch(deleteEvent(event.id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventItem);
