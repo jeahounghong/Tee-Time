0b2278ac3756bfc9069fadd81a39cf158f8caea0
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-09-01 15:12:55 -0700

    profile skeleton

---
+++ frontend/src/components/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='about-page-container'>
+        <div className="dev-profile-container">
+            <div className="dev-profile-item">
+                <div className="dev-profile-image"></div>
+                <div className="dev-profile-information"></div>
+                <div className="dev-profile-links"></div>
+            </div>
+        </div>
+    </div>
+  )
+};
