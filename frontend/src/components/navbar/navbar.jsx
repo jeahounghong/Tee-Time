aea2c1d9f737477dae543aca935c5289c4910a0b
Michael Shen <michaelshen85@gmail.com>
2022-08-31 23:46:20 -0400

    Add new navbar icons

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGolfBallTee, faCalendar, faUsers, faCircleUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { GrGroup } from 'react-icons/gr';
+import { BsCalendarDate } from 'react-icons/bs';
+import { BiExit } from 'react-icons/bi';
+import { AiOutlineUser } from 'react-icons/ai';
 import '../../stylesheets/nav.css';
 import React from 'react';
 
@@ -15,16 +19,16 @@ class Navbar extends React.Component {
             return (
                 <div id="session-links">
                     <Link className="session-link" to="/events">
-                        <div className="session-link-icon"><FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon></div>
+                        <div className="session-link-icon"><BsCalendarDate /></div>
                     </Link>
                     <Link className="session-link" to="/groups">
-                        <div className="session-link-icon"><FontAwesomeIcon icon={faUsers}></FontAwesomeIcon></div>
+                        <div className="session-link-icon"><GrGroup /></div>
                     </Link>
                     <Link className="session-link" to={`/users/${this.props.currentUser.id}`}>
-                        <div className="session-link-icon"><FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon></div>
+                        <div className="session-link-icon"><AiOutlineUser /></div>
                     </Link>
                     <Link className="session-link" to="/">
-                        <div onClick={this.props.logout} className="session-link-icon"><FontAwesomeIcon icon={faRightFromBracket}></FontAwesomeIcon></div>
+                        <div onClick={this.props.logout} className="session-link-icon"><BiExit /></div>
                     </Link>
                 </div>
             )
