0b1de325b4458e7ca209c8878a6d248648a9d120
Michael Shen <michaelshen85@gmail.com>
2022-09-01 09:44:19 -0400

    Fix navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -23,40 +23,20 @@ class Navbar extends React.Component {
 
     sessionLinks() {
         const tempUser = this.props.users[this.props.currentUser.id]
-        if (tempUser){
-            return (
-                <div id="session-links">
-                    <Link className="session-link" to="/events">
-                        <div className="session-link-icon"><FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon></div>
-                    </Link>
-                    <Link className="session-link" to="/groups">
-                        <div className="session-link-icon"><FontAwesomeIcon icon={faUsers}></FontAwesomeIcon></div>
-                    </Link>
-                    <Link className="session-link" to={`/users/${this.props.currentUser.id}`}>
-                        <div className="session-link-pro-pic"><img src={tempUser.imageUrl} /></div>
-                    </Link>
-                    <Link className="session-link" to="/">
-                        <div onClick={this.props.logout} className="session-link-icon"><FontAwesomeIcon icon={faRightFromBracket}></FontAwesomeIcon></div>
-                    </Link>
+        return (
+            <div id="session-links">
+                <Link className="session-link" to="/events">
+                    <div className="session-link-icon">Events</div>
+                </Link>
+                <Link className="session-link" to="/groups">
+                    <div className="session-link-icon">Groups</div>
+                </Link>
+                <div className="session-link-profile" onClick={this.toggleProfileModal}>
+                    {/* {this.props.currentUser ? this.props.currentUser.firstName.slice(0,1) : ""} */}
+                    {tempUser ? <img src={tempUser.imageUrl} /> : ""}
                 </div>
-            )
-        } else {
-            return (
-                <div id="session-links">
-                    <Link className="session-link" to="/events">
-                        <div className="session-link-icon">Events</div>
-                    </Link>
-                    <Link className="session-link" to="/groups">
-                        <div className="session-link-icon">Groups</div>
-                    </Link>
-                    <div className="" to={`/users/${this.props.currentUser.id}`}>
-                        <div className="session-link-profile" onClick={this.toggleProfileModal}>
-                            {/* {this.props.currentUser ? this.props.currentUser.firstName.slice(0,1) : ""} */}
-                        </div>
-                    </div>
-                </div>
-            )
-        }
+            </div>
+        )
     }
 
     render() {
