93eeffe695c362db928d7114fcb627693aa43d4a
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-09-01 16:24:25 -0700

    redid about page

---
+++ frontend/src/components/about/about 2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='about-page-container'>
+        <div className="dev-profile-container">
+            
+        </div>
+    </div>
+  )
+};
