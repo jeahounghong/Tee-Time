b6d5005539595fb757f39e00319b7428ba2b49e2
“Jeahoung <jeahounghong@gmail.com>
2022-09-06 09:55:04 -0400

    add Jeahoung angelist Link

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -56,7 +56,7 @@ class About extends React.Component {
                                 <a href="https://github.com/jeahounghong"><img className='dev-profile-icon' src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" /></a>
                                 <a href="https://www.linkedin.com/in/david-jeahoung-hong-7ab00b134/"><img className='dev-profile-icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png" alt="" /></a>
                                 <a href=""><FontAwesomeIcon className="dev-profile-icon" icon={faUserCircle}></FontAwesomeIcon></a>
-                                <a href="https://angel.co/u/kyung-koo"><img className='dev-profile-icon' src="https://cdn1.iconfinder.com/data/icons/logos-and-brands-3/512/20_Angellist_logo_logos-512.png" alt="" /></a> 
+                                <a href="https://angel.co/u/david-jeahoung-hong`"><img className='dev-profile-icon' src="https://cdn1.iconfinder.com/data/icons/logos-and-brands-3/512/20_Angellist_logo_logos-512.png" alt="" /></a> 
                             </div>
                         </div>
                         <div className="dev-profile-item">
