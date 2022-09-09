133e6a4595f55fe4058d01d17dacaca44e9efc1c
“Jeahoung <jeahounghong@gmail.com>
2022-09-09 11:49:13 -0400

    store fix

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -8,7 +8,8 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, (process.env.REACT_APP_MAP_KEY ? "" : logger))
+    applyMiddleware(thunk),
+    // applyMiddleware(thunk, logger)
   )
 );
 
