dce1a11ba359e0675aca06d18e5ec968822a33af
“Jeahoung <jeahounghong@gmail.com>
2022-09-12 11:05:17 -0400

    render logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -8,8 +8,8 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk),
-    // applyMiddleware(thunk, logger)
+    // applyMiddleware(thunk),
+    applyMiddleware(thunk, logger)
   )
 );
 
