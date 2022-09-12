9040ef815bedaa6fb3ea07c8b6bb0eed188e42f2
Torben Ginsberg <tginsberg8@gmail.com>
2022-09-12 11:42:14 -0400

    update session actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -57,8 +57,7 @@ export const login = user => dispatch => (
         APIUtil.setAuthToken(token);
         const decoded = jwt_decode(token);
         dispatch(receiveCurrentUser(decoded))
-    })
-    .catch(err => {
+    }, err => {
         dispatch(receiveErrors(err.response.data));
     })
 )
