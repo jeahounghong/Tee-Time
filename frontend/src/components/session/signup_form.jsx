0f0eac08dbd27003a7d3a6d9419494168b5311bc
Torben Ginsberg <tginsberg8@gmail.com>
2022-09-12 13:15:19 -0400

    fix session error rendering

--- frontend/src/components/session/signup_form.jsx
+++ frontend/src/components/session/signup_form.jsx
@@ -15,6 +15,8 @@ class SignUpForm extends React.Component {
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.update = this.update.bind(this);
+        this.renderErrors = this.renderErrors.bind(this);
+        this.clearErrors = this.clearErrors.bind(this);
     }
 
     componentDidMount() {
@@ -40,6 +42,47 @@ class SignUpForm extends React.Component {
         return e => this.setState({[field]: e.target.value})
     }
 
+    renderErrors(field) {
+        switch(field) {
+            case 'firstName':
+                return (
+                    <div className="firstName-errors">
+                        {this.props.errors.firstName}
+                    </div>
+                );
+            case 'lastName':
+                return (
+                    <div className="lastName-errors">
+                        {this.props.errors.lastName}
+                    </div>
+                );
+            case 'email':
+                return (
+                    <div className="email-signup-errors">
+                        {this.props.errors.email}
+                    </div>
+                );
+            case 'password':
+                return (
+                    <div className="password-signup-errors">
+                        {this.props.errors.password}
+                    </div>
+                );
+            case 'password2':
+                return (
+                    <div className="password2-errors">
+                        {this.props.errors.password2}
+                    </div>
+                );
+            default: 
+                    return (<div className="default"></div>)
+        }
+    }
+
+    clearErrors() {
+        this.props.clearSessionErrors();
+    }
+
     render() {
         return (
             <div className="session-form-container">
@@ -56,27 +99,27 @@ class SignUpForm extends React.Component {
                     </div>
                     <form onSubmit={this.handleSubmit}>
                         <div className="signup-form-input">
-                            <label>First Name:</label>
+                            <label>First Name: {this.renderErrors('firstName')}</label>
                             <input type="text" value={this.state.firstName} onChange={this.update('firstName')}/>
                         </div>
                         <div className="signup-form-input">
-                            <label>Last Name:</label>
+                            <label>Last Name: {this.renderErrors('lastName')}</label>
                             <input type="text" value={this.state.lastName} onChange={this.update('lastName')}/>
                         </div>
                         <div className="signup-form-input">
-                            <label>Email:</label>
+                            <label>Email: {this.renderErrors('email')}</label>
                             <input type="text" value={this.state.email} onChange={this.update('email')}/>
                         </div>
                         <div className="signup-form-input">
-                            <label>Password:</label>
+                            <label>Password: {this.renderErrors('password')}</label>
                             <input type="password" value={this.state.password} onChange={this.update('password')}/>
                         </div>
                         <div className="signup-form-input">
-                            <label>Confirm Password:</label>
+                            <label>Confirm Password: {this.renderErrors('password2')}</label>
                             <input type="password" value={this.state.password2} onChange={this.update('password2')}/>
                         </div>
                         <div className="login-form-footer">
-                        <p id="signin-footer-info">Already have an account? <Link id="signup-link" to="/login">Login</Link></p>
+                        <p id="signin-footer-info">Already have an account? <Link onClick={this.clearErrors} id="signup-link" to="/login">Login</Link></p>
                             <button id="signup-btn" type="login">Sign up</button>
                         </div>
                     </form>
