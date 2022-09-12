bb3f2671521f607f019d15a4a7a2d15a08c243f8
“Jeahoung <jeahounghong@gmail.com>
2022-09-12 14:30:29 -0400

    add personal links

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -49,14 +49,14 @@ class About extends React.Component {
                                     <div className="dev-profile-title">Backend Flex</div>
                                 </div>
                                 <div className="dev-profile-image">
-                                    <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGWz2A2orT5uA/profile-displayphoto-shrink_800_800/0/1552154308118?e=1667433600&v=beta&t=NvMqCR4vbtOF9HrUZ_EhJ3BBzFHozAad1hlOow2L--E" alt="" />
+                                    <img src="https://avatars.githubusercontent.com/u/104883249?v=4" alt="" />
                                 </div>
                             </div>
                             <div className="dev-profile-links">
                                 <a href="https://github.com/jeahounghong"><img className='dev-profile-icon' src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" /></a>
                                 <a href="https://www.linkedin.com/in/david-jeahoung-hong-7ab00b134/"><img className='dev-profile-icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png" alt="" /></a>
-                                <a href=""><FontAwesomeIcon className="dev-profile-icon" icon={faUserCircle}></FontAwesomeIcon></a>
-                                <a href="https://angel.co/u/david-jeahoung-hong`"><img className='dev-profile-icon' src="https://cdn1.iconfinder.com/data/icons/logos-and-brands-3/512/20_Angellist_logo_logos-512.png" alt="" /></a> 
+                                <a href="https://jeahounghong.github.io/Portfolio/"><FontAwesomeIcon className="dev-profile-icon" icon={faUserCircle}></FontAwesomeIcon></a>
+                                <a href="https://angel.co/u/david-jeahoung-hong"><img className='dev-profile-icon' src="https://cdn1.iconfinder.com/data/icons/logos-and-brands-3/512/20_Angellist_logo_logos-512.png" alt="" /></a> 
                             </div>
                         </div>
                         <div className="dev-profile-item">
